bb42ac07b6fa33b8d0618ce273200951a6d51f07
k9ordon <github@k94n.com>
2015-11-16 22:16:21 +0100

    scrobble & cleanup

--- main.js
+++ main.js
@@ -1,37 +1,36 @@
 var player = require('chromecast-player')();
-var spotify = require('spotify');
 var scribble = require('scribble');
 var config = require('./config.json');
 
-console.log(config.lastfm.key,config.lastfm.secret,config.lastfm.username,config.lastfm.password);
-
 var Scrobbler = new scribble(config.lastfm.key,config.lastfm.secret,config.lastfm.username,config.lastfm.password);
-
 
 player.attach(function(err, p) {
     var session = p.currentSession;
-    console.log(session.playerState, session.media.contentType);
+
+    console.log(session.playerState);
 
     p.on('status', onStatus);
 });
 
 var onStatus = function(status) {
-    console.log('\n\nstatus changed', status);
+    //console.log('\n\nstatus changed', status);
 
-    if(status.playerState == "PLAYING"
-        && status.media.contentType == "application/x-spotify.track") {
-        console.log('spotify track',
+    if(status.playerState == "PLAYING" && status.media.metadata) {
+
+        console.log('now playing: ',
             status.media.metadata.songName,
             status.media.metadata.artist,
             status.media.metadata.albumName
         );
 
-/*
-        spotify.lookup({type:'track', id:status.media.contentId.replace('spotify:track:','')},function(err, data) {
-            if(data) {
-                console.log('Now Playing: ', data.name, data.artist, data.albumName);
-            }
-        })
-*/
+        var song = {
+            artist: status.media.metadata.artist,
+            track: status.media.metadata.songName,
+            album: status.media.metadata.albumName
+        };
+
+        Scrobbler.Scrobble(song, function(post_return_data) {
+            //console.log('Scrobble', post_return_data);
+        });
     }
 }
