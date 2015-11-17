9d7903ad5569521a1db17f71e6dd2d416201e0e5
k9ordon <github@k94n.com>
2015-11-17 22:52:16 +0100

    fixes, http status

--- main.js
+++ main.js
@@ -4,33 +4,45 @@ var config = require('./config.json');
 
 var Scrobbler = new scribble(config.lastfm.key,config.lastfm.secret,config.lastfm.username,config.lastfm.password);
 
+var STATUS = false;
+var LAST_TRACK = false;
+var LAST_LAST_FM_RESPONSE = false;
+
+
 player.attach(function(err, p) {
     var session = p.currentSession;
-
-    console.log(session.playerState);
-
+    STATUS = session.playerState;
     p.on('status', onStatus);
 });
 
 var onStatus = function(status) {
-    //console.log('\n\nstatus changed', status);
+    STATUS = status.playerState;
 
-    if(status.playerState == "PLAYING" && status.media.metadata) {
-
+    if(status.playerState == "PLAYING" && status.media && status.media.metadata) {
+/*
         console.log('now playing: ',
             status.media.metadata.songName,
             status.media.metadata.artist,
             status.media.metadata.albumName
         );
-
+*/
         var song = {
             artist: status.media.metadata.artist,
             track: status.media.metadata.songName,
             album: status.media.metadata.albumName
         };
 
-        Scrobbler.Scrobble(song, function(post_return_data) {
-            //console.log('Scrobble', post_return_data);
+        LAST_TRACK = song.artist + ' - ' + song.track;
+
+        Scrobbler.Scrobble(song, function(response) {
+            LAST_LAST_FM_RESPONSE = response;
         });
     }
 }
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(STATUS + '\n' + LAST_TRACK + '\n' + LAST_LAST_FM_RESPONSE);
+}).listen(8123);
